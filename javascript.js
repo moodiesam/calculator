ad29b22bc687b0baf6f42b63a6a762df84062370
moodiesam <sampmcampbell@gmail.com>
2023-05-27 14:38:04 -0400

    create the calculator in html and css

---
+++ javascript.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+var firstNumber = '';
+var secondNumber = '';
+var operator = null;
+
+function operate(operator, a, b) {
+   
+    a = Number(a);  
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return null
+            else return divide(a, b);
+        default:
+            return null;
+        
+    };
+};
